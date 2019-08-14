30f33ddaf4820a9e45ffa86a6aeabd88bb9aaaa5
Nasser Alhatlan <nasseralhatlan@Nassers-MacBook-Pro.local>
2019-08-14 18:55:51 +0300

    omar

--- src/pages/signup.php
+++ src/pages/signup.php
@@ -1,23 +1,43 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <?php
+           include('links.php'); 
+      ?>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+ 
+    <link rel="stylesheet" href="../assets/css/signup.css" >
     <title>Sign up</title>
   </head>
   <body>
 
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <div class="main-card">
+              <div class="field-div">
+                   <label>E-MAIL</label>
+                   <input placeholder="email"type="text" >
+             </div>
+
+             <div class="field-div">
+                   <label>NAME</label>
+                   <input placeholder="company name"type="text" >
+             </div>
+
+
+             <div class="field-div">
+                   <label>PASSWORD</label>
+                   <input placeholder="Password" type="password" >
+             </div>
+
+             <div class="field-div">
+                   <label>RE-ENTER PASSWORD</label>
+                   <input placeholder="Re enter password" type="password" >
+             </div>
+
+             <!-- <button type="button" class="btn btn-info">Sign up</button> -->
+
+
+    </div>
   </body>
 </html>
