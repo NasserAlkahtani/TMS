672a4e3f6fc0b213483063d45dc0b0ebff39cdca
Nasser Alhatlan <nasseralhatlan@Nassers-MacBook-Pro.local>
2019-08-14 19:13:44 +0300

    Sign up page css

--- src/pages/signup.php
+++ src/pages/signup.php
@@ -14,28 +14,31 @@
 
 
     <div class="main-card">
+
+
+    <h1>Sign up</h1>
               <div class="field-div">
-                   <label>E-MAIL</label>
+                   <label>E-Mail</label>
                    <input placeholder="email"type="text" >
              </div>
 
              <div class="field-div">
-                   <label>NAME</label>
+                   <label>Name</label>
                    <input placeholder="company name"type="text" >
              </div>
 
 
              <div class="field-div">
-                   <label>PASSWORD</label>
+                   <label>Password</label>
                    <input placeholder="Password" type="password" >
              </div>
 
              <div class="field-div">
-                   <label>RE-ENTER PASSWORD</label>
+                   <label>Re enter password</label>
                    <input placeholder="Re enter password" type="password" >
              </div>
 
-             <!-- <button type="button" class="btn btn-info">Sign up</button> -->
+             <button type="button" class="signup-btn">Sign up</button>
 
 
     </div>
