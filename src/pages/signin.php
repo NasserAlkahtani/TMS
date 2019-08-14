f4f7b22615d34bbcc2c135e28cf8fa8d95906ece
Omar ALqahtani <beef@Omars-MacBook-Air.local>
2019-08-14 19:18:47 +0300

    signin page

--- src/pages/signin.php
+++ src/pages/signin.php
@@ -1,23 +1,35 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <?php
+           include('links.php'); 
+      ?>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+ 
+    <link rel="stylesheet" href="../assets/css/signin.css" >
+    <title>Sign up</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  
-</body>
+
+
+    <div class="main-card">
+              <h1>Sign In</h1>
+
+             <div class="field-div">
+                   <label>Name</label>
+                   <input placeholder="company name"type="text" >
+             </div>
+
+
+             <div class="field-div">
+                   <label>Password</label>
+                   <input placeholder="Password" type="password" >
+             
+
+             <button type="button" class="btn btn-info">Sign in</button>
+
+
+    </div>
+  </body>
 </html>
