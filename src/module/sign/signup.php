f3b760474ca1b07e24da9dff2ec45eae1aa1193e
Omar ALqahtani <beef@Omars-MacBook-Air.local>
2019-08-16 01:29:06 +0300

    refactoring signup.php

--- src/module/sign/signup.php
+++ src/module/sign/signup.php
@@ -15,24 +15,38 @@ $sqlCheckEmail = "SELECT * FROM `companies` WHERE email = '".$email."' ";
 
 $resultCheckEmail = mysqli_query($conn,$sqlCheckEmail);
 
+session_start();
+
 if(mysqli_num_rows($resultCheckEmail) > 0){
-   session_start();
    $_SESSION['msg']  = "Email already exist!!";
    $_SESSION['msg-type'] = "danger";
-}else{
-  if($repassword == $password){
+   header('location: ../../pages/sign.php');
+}
+
+
+
+
+  if($repassword != $password){
+    $_SESSION['msg'] =  "the passwords do not match!!";
+    $_SESSION['msg-type'] = "danger";
+    header('location: ../../pages/sign.php');
+ } 
+
+
     $sql = "INSERT INTO `companies` (`id`, `name`, `email`, `password`, `logo`) VALUES (NULL, '".$name."', '".$email."', '".$password."', NULL);";
     $result = mysqli_query($conn,$sql);
-    if($result){
+
+    if(!$result){
+        $_SESSION['msg'] =  "Something went worng try again later";
+        $_SESSION['msg-type'] = "danger";
         header('location: ../../pages/sign.php');
     }
-  }else{
-      session_start();
-      $_SESSION['msg'] =  "the passwords do not match!!";
-      $_SESSION['msg-type'] = "danger";
+
       
-}
-}
+    header('location: ../../pages/sign.php');
+
+
+
 
 
 
