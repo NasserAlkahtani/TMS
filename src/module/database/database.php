aeb5eee259c0d318fd3299cfb478851c6dbcd271
Nasser Alhatlan <nasseralhatlan@Nassers-MacBook-Pro.local>
2019-08-16 20:01:00 +0300

    Update database connection

--- src/module/database/database.php
+++ src/module/database/database.php
@@ -4,8 +4,6 @@
         $password = "";
         $dbname = "TMS";
 
-        $Beef;
-
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
         $sSQL= 'SET CHARACTER SET utf8'; 
--- src/pages/sign.php
+++ src/pages/sign.php
@@ -18,7 +18,6 @@
   if(isset($_SESSION['msg']) && isset($_SESSION['msg-type']) ){
 
 
-
   }
 
 ?>
@@ -43,16 +42,13 @@
                    <label>Name</label>
                    <input placeholder="company name" type="text" name="name" required>
              </div>
-             
-             <div class="field-div-row">
-                   <div class="field-div-row-div">
+             <div class="field-div">
                    <label>Password</label>
                    <input placeholder="Password" type="password" name="password" required>
-                   </div>
-                   <div class="field-div-row-div">
-                   <label>Conform password</label>
+             </div>
+             <div class="field-div">
+                   <label>Re enter password</label>
                    <input placeholder="Re enter password" type="password" name="repassword" required>
-                   </div>
              </div>
              <button type="submit" class="sign-btn">Sign up</button>
      </form>
