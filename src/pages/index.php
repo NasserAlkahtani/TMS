cb726d1190070081dc5f02e820153a953c8cbec2
Nasser Alhatlan <nasseralhatlan@Nassers-MacBook-Pro.local>
2019-08-18 01:43:10 +0300

    Started index

--- src/pages/index.php
+++ src/pages/index.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+    <link href="../assets/css/notification.css" rel="stylesheet">
+    <?php
+           include('links.php'); 
+     ?>
 </head>
 <body>
     
