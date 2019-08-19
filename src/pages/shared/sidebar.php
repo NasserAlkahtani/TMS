03e7f0ec431358dc5cb88b2b252de332cb822fff
Omar ALqahtani <beef@Omars-MacBook-Air.local>
2019-08-19 04:07:48 +0300

    sidebar company info added

--- src/pages/shared/sidebar.php
+++ src/pages/shared/sidebar.php
@@ -1,4 +1,27 @@
 <div class="main-sidebar">
+    
 
+<div class="company-name-logo">
+<?php
+   include("../module/database/database.php"); 
+
+   $sql = 'SELECT `logo` FROM `companies` WHERE id="12"';
+   $res = mysqli_query($conn,$sql);
+
+   while($row = mysqli_fetch_assoc($res)){
+   echo '
+   <img class="company-logo" src="data:image/jpeg;base64,'.base64_encode($row['logo']).'" >
+   ';
+}
+?>
+ <div class="company-info">
+    <div class="company-name">Hat tech</div>
+    <div class="company-date"> 18th october 2019</div>
+  </div>
+</div>
+
+   
+   
+<div class="line"></div>
 
 </div>
