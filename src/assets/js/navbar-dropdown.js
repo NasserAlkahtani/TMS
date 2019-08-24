42d9f24f1ccfdf1a4a16db3e002355ef7e9743a3
Nasser Alhatlan <nasseralhatlan@Nassers-MacBook-Pro.local>
2019-08-24 20:12:40 +0300

    my changes

--- src/assets/js/navbar-dropdown.js
+++ src/assets/js/navbar-dropdown.js
@@ -1,21 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
   function remove() {
-    $("#off-icon-dropdown-div").fadeOut();
+    $("#off-icon-dropdown-div").fadeOut(200);
     $("#off-icon-dropdown-div-cover").removeClass(
       "off-icon-dropdown-div-cover"
     );
   }
+
   function active() {
     $("#off-icon-dropdown-div").show();
     $("#off-icon-dropdown-div-cover").addClass("off-icon-dropdown-div-cover");
   }
 
-  $("#off-icon-dropdown-div-cover").click(function() {
+  $("#off-icon-dropdown-div-cover").click(function () {
     console.log("aa");
     remove();
   });
 
-  $("#dropdown-button").click(function() {
+  $("#dropdown-button").click(function () {
     if (
       $("#off-icon-dropdown-div").attr("style") == "display: none;" ||
       $("#off-icon-dropdown-div").attr("style") == undefined
